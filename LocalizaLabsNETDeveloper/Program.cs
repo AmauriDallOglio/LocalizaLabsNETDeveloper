51aa3ad8d5d1c6f132b0627d7bc522680b0a4645
Amauri DallOglio <amauridalloglio@outlook.com.br>
2023-08-26 15:09:24 -0300

    Adicionar arquivos de projeto.

---
+++ LocalizaLabsNETDeveloper/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using LocalizaLabsNETDeveloper.Criando_uma_Aplicação_de_Transferências_Bancárias_com_NET_Transferências_Bancárias;
+using LocalizaLabsNETDeveloper.Desafios;
+
+//Console.WriteLine("Consumo_Médio_do_Automóvel");
+//var Consumo_Médio_do_Automóvel = new Consumo_Médio_do_Automóvel();
+//var distancia = Convert.ToInt32(Console.ReadLine());
+//double combustivelGasto = Convert.ToDouble(Console.ReadLine());
+//var resultado = Consumo_Médio_do_Automóvel.Resultado(distancia, combustivelGasto);
+//Console.WriteLine("{0:0.000} km/l", resultado);
+
+//Console.WriteLine("Digite 11");
+//var DDD = new DDD();
+//int ddd = Convert.ToInt32(Console.ReadLine());
+//DDD.Resultado(ddd);
+//Console.WriteLine("DDD");
+
+var banco = new Banco();
+banco.Main();
+
+Console.WriteLine("DDD");
